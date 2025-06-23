df9a740a5fa555e26bab6eed6ed7d6b9f0dee631
spinstein <2717896+spinstein@users.noreply.github.com>
2025-06-23 20:01:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,7 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Add necessary feed sources
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default  # For OpenClash
+echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
+echo 'src-git qinglong https://github.com/whyour/qinglong' >>feeds.conf.default  # For Qinglong
