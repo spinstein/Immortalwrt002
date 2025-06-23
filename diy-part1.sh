9098ab1b64123abca83543b4ca4ce7507eb972f5
spinstein <2717896+spinstein@users.noreply.github.com>
2025-06-23 20:17:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,4 @@
 # Add necessary feed sources
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default  # For OpenClash
 echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
-echo 'src-git qinglong https://github.com/whyour/qinglong' >>feeds.conf.default  # For Qinglong
+echo 'src-git qinglong https://github.com/brvphoenix/luci-app-qinglong' >>feeds.conf.default
